1d80525984de65bc2c7cef84a4b8e58aaab26f82
Rodrigo Medeiros <rodrigomdros@turno.com>
2023-10-13 23:13:23 -0300

    chore: add relation to customer

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
@@ -32,5 +33,10 @@ class Order extends Model
     public function items()
     {
         return $this->belongsToMany(Item::class, 'order_item', 'order_id', 'item_id');
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
     }
 }
